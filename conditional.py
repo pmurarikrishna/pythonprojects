8b96192fd869467e5742d7c42c0b9e1246b198b2
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-06-22 03:48:26 +0530

    commit.

---
+++ conditional.py
@@ -0,0 +1,17 @@
+num=int(input("enter no "))
+print(num)
+# if(num%2 !=0):
+#  print( "weird")
+# elif():
+if(num%2==0 and 2<=num<=5):
+       print("not weird")
+# elif():
+if(num%2==0 and 6<=num<=20):
+        print("weird")
+    #  elif():
+if(num%2==0 and num>20):
+            print("not weird")
+if(num%2!=0):
+                print("weird")
+
+                print ("done")
