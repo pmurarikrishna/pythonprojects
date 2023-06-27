d2cde02f09a447afcb866077ee112a66c38cc0b1
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-06-28 04:34:40 +0530

    commit.

---
+++ ques1.py
@@ -0,0 +1,49 @@
+#get an element in set and implement all the operations in your ide 
+#illustrate the importancce of collection modules
+# implement atlest four functions of collecton modules '
+
+
+from ctypes import sizeof
+
+
+myset={"krishna ",774,"vimal",763, 766,1 ,2 ,4,}
+print(myset)
+print(type(myset)) 
+myset.add("parth")
+print(myset) 
+# try:       
+for i in range(1,3):
+    input1=int(input("enter"))
+    myset.add(input1)
+
+    print(myset)
+         
+         
+# except :
+#     print(myset)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    )
+# myset.remove("vimal")
+# print(myset)
+
+# myset2={6,7,8}
+
+
+# print(myset2)
+
+# set=myset.union(myset2)
+# print (set)
+
+
+# print(bool(myset2))
+
+# # intesection in sets 
+
+# print("intersection set")
+ 
+# S=myset & myset2
+# print(S )
+
+# printong length of sets 
+
+# print(sizeof (intersectionset))
+
+
