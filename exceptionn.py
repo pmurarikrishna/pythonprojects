b18da986a3c1f4ba9f3f8371ed73f737e45b908f
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-07-04 04:51:28 +0530

    commit.

---
+++ exceptionn.py
@@ -0,0 +1,13 @@
+try:
+    a=int(input("enter a"))
+    b=int(input("enter b"))
+    c=a/b
+
+except ZeroDivisionError:
+    print("can not divide by zero")
+    print("exception")
+except ValueError:
+    print("value error ")
+    #define a program to show diffferrent type of exceptional error
+    # regular expression to find matcin text  
+
