fde42a3cb7e35a230d932a60613c5728841f90f3
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-07-08 04:57:29 +0530

    commit.

---
+++ class - Copy.py
@@ -0,0 +1,12 @@
+class Phone:
+    def makecall(self):
+     print ("make call")
+    def playgames(self):
+        print ("games playing ")
+# this is the way to create object 
+oneplus=Phone()
+samsung=Phone()
+
+oneplus.makecall()
+samsung.playgames()
+oneplus.playgames()
