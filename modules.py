b37517ece5dec8500662b57ee5bd0524ad03c561
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-10-04 03:51:12 +0530

    commit.

---
+++ modules.py
@@ -0,0 +1,14 @@
+import imp
+import math  
+a=int(input("enter your no  "  ))
+print(math.factorial(a))
+ # importin with renaming 
+import math as m 
+x=m.pi
+print ("the value of pi ", x )
+
+a=2 
+print("id(2)=" , id(2))
+print("id(a)=" , id(a))
+
+
