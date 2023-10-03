11a849ae17654591a9b23d8a961289461e514b16
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-10-04 03:54:20 +0530

    commit.

---
+++ max.py
@@ -0,0 +1,26 @@
+from cgi import print_directory
+
+
+x=int(input("enter the vaule 1 ="))
+y=int(input("enter the vaule 1 ="))
+z=int(input("enter the vaule 1 ="))
+# f=int(input("choice"))10
+8
+
+def maximum(a, b, c):
+  
+    if (a >= b) and (a >= c):
+        largest = a
+  
+    elif (b >= a) and (b >= c):
+        largest = b
+    else:
+        largest = c
+          
+    return largest
+
+print(maximum)
+
+
+
+    
